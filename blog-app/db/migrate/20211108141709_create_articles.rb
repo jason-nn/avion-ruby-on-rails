2d654f289035a92eb051673fa42cfd090c668f64
jason-nn <jason.ho@obf.ateneo.edu>
2021-11-09 21:12:54 +0800

    class exercises

--- blog-app/db/migrate/20211108141709_create_articles.rb
+++ blog-app/db/migrate/20211108141709_create_articles.rb
@@ -1,8 +1,10 @@
 class CreateArticles < ActiveRecord::Migration[6.1]
-  def change
-    create_table :articles do |t|
+    def change
+        create_table :articles do |t|
+            t.string :name
+            t.text :body
 
-      t.timestamps
+            t.timestamps
+        end
     end
-  end
 end
