ff3b4117582e68db7929f2ca9f30b0887f4e852e
jason-nn <jason.ho@obf.ateneo.edu>
2021-11-19 19:03:54 +0800

    finished tutorial

--- tutorial/app/models/article.rb
+++ tutorial/app/models/article.rb
@@ -1,4 +1,7 @@
 class Article < ApplicationRecord
+    include Visible
+    has_many :comments, dependent: :destroy
+
     validates :title, presence: true, uniqueness: true
     validates :body, presence: true, length: { minimum: 10 }
 end
