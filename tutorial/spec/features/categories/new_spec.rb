e0b288d68fb3faaa6a906a2fe0a0000b3454d9c5
jason-nn <jason.ho@obf.ateneo.edu>
2021-11-20 15:31:01 +0800

    added new spec

---
+++ tutorial/spec/features/categories/new_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe 'Category Feature: Edit', type: :feature do
+    it 'should show the new category header' do
+        visit new_category_path
+        expect(page).to have_content ('New Category')
+    end
+
+    it 'should go to the all categories path when all categories is clicked' do
+        visit new_category_path
+        click_on 'All Categories'
+        expect(page).to have_current_path categories_path
+    end
+end
