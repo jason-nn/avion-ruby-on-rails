2669b9d4eec32bcb0bd730e657053cbfc558a1c6
jason-nn <jason.ho@obf.ateneo.edu>
2021-11-22 21:01:32 +0800

    added request specs

---
+++ tutorial/spec/requests/categories_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe 'Category Requests', type: :request do
+    it 'GET /categories' do
+        get '/categories'
+        expect(response).to have_http_status(200)
+    end
+
+    it 'POST /categories' do
+        expect do
+            post categories_path, params: { category: { name: 'Test' } }
+        end.to change(Category, :count).by(1)
+    end
+
+    it 'GET /categories/new' do
+        get '/categories/new'
+        expect(response).to have_http_status(200)
+    end
+
+    it 'GET /categories/:id/edit' do
+        category = Category.create(name: 'Test')
+        get edit_category_path(category)
+        expect(response).to have_http_status(200)
+    end
+
+    it 'GET /articles/:id' do
+        category = Category.create(name: 'Test')
+        get category_path(category)
+        expect(response).to have_http_status(200)
+    end
+end
